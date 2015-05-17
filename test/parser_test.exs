740dbe1cb39b0c550d31f02a7b4599e22846e495
Florian J. Breunig <Florian.J.Breunig@my-flow.com>
2015-05-18 01:23:58 +0200

    add more assertions to increase code coverage

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -5,14 +5,14 @@ defmodule ParserTest do
 
 
   test "invalid format, no key found" do
-    assert {:error, :badarg} ==
-      "asdfghjkl" |> parse
+    assert {:error, :badarg} == "asdfghjkl" |> parse
+    assert_raise ArgumentError, fn -> "asdfghjkl" |> parse! end
   end
 
 
   test "invalid format, key does not match expected length" do
-    assert {:error, :badarg} ==
-      ":20STARTUMS:25:74061813/0100033626" |> parse
+    assert {:error, :badarg} == ":20STARTUMS:25:74061813/0100033626" |> parse
+    assert_raise ArgumentError, fn -> ":20STARTUMS:25:74061813/0100033626" |> parse! end
   end
 
 

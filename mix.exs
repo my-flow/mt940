9c2ce1355364e81253511324bcad516ccc1276f0
Florian J. Breunig <Florian.J.Breunig@my-flow.com>
2015-09-16 00:37:00 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mt940.Mixfile do
   def project do
     [
       app: :mt940,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.0",
       description: description,
       package: package,
